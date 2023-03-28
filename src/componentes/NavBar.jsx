bc7efcd2ae7a8dfd546cbb0aa7cbe7c3ad9d7b33
Enzofelip <enzo.felipebb78@gmail.com>
2023-03-28 16:50:07 -0300

    atsProjetoAxios

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -12,10 +12,22 @@ const NavBar = () => {
         e.preventDefault();
         setDados(false)
     }
+    const homeBtn = () => {
+        setDados(false)
+    }
+    const newBtn = () => {
+        setDados(false);
+    }
+    const geBtn = () => {
+        setDados(false);
+    }
+    const bloBtn = () => {
+        setDados(false);
+    }
     return(
         <div className="navbar">
             <div className="nav-texto">
-                <h2><Link to={"/"}>Blog</Link></h2>
+                <h2><Link onClick={bloBtn} to={"/"}>Blog</Link></h2>
 
                 <ul>
                     <li><Link to={"/"}>Home</Link></li>
@@ -48,9 +60,9 @@ const NavBar = () => {
                     <h2><Link to={"/"}>Blog</Link></h2>
                     <div className="ullist">
                         <ul>
-                            <li><Link to={"/"}>Home</Link></li>
-                            <li><Link to={"/new"} className="newbtn">New Post</Link></li>
-                            <li><Link to={"/admin"}>Gerenciar</Link></li>
+                            <li><Link to={"/"} onClick={homeBtn}>Home</Link></li>
+                            <li><Link to={"/new"} onClick={newBtn} className="newbtn">New Post</Link></li>
+                            <li><Link to={"/admin"} onClick={geBtn}>Gerenciar</Link></li>
                         </ul>
                     </div>
                    
